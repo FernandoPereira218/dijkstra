459f8bccf5ca15e0ebd2843b513c5b5a0a9c8c2a
FernandoPereira218 <fernandofmp218@gmail.com>
2022-06-02 20:52:57 -0300

    ler dados de txt

--- src/2/main.py
+++ src/2/main.py
@@ -2,10 +2,33 @@
 from Dijkstra import Dijkstra
 from Graph import Graph
 # nodes = ["Reykjavik", "Oslo", "Moscow", "London", "Rome", "Berlin", "Belgrade", "Athens"]
-nodes = ["J", "A", "P", "Q", "B", "C", "E"]
-init_graph = {}
-for node in nodes:
-    init_graph[node] = {}
+
+with open('graph.txt') as txt:
+    lines = txt.readlines()
+
+# for line in lines:
+#     temp = line.replace('\n', '')
+#     nodes = temp.split(';')
+#     print(nodes)
+
+temp = lines[0].replace('\n', '')
+nodes = temp.split(';')
+print(nodes)
+
+new_graph = {}
+for n in nodes:
+    new_graph[n] = {}
+
+for i in range(2, len(lines)):
+    temp = lines[i].replace('\n', '').split(';')
+    new_graph[temp[0]][temp[1]] = int(temp[2])
+
+print(new_graph)
+
+# nodes = ["J", "A", "P", "Q", "B", "C", "E"]
+# init_graph = {}
+# for node in nodes:
+#     init_graph[node] = {}
     
 # init_graph["Reykjavik"]["Oslo"] = 5
 # init_graph["Reykjavik"]["London"] = 4
@@ -16,33 +39,34 @@ for node in nodes:
 # init_graph["Athens"]["Belgrade"] = 1
 # init_graph["Rome"]["Berlin"] = 2
 # init_graph["Rome"]["Athens"] = 2
-init_graph["J"]["A"] = 5
-init_graph["J"]["P"] = 6
-init_graph["J"]["Q"] = 10
-init_graph["A"]["J"] = 5
-init_graph["A"]["B"] = 13
-init_graph["P"]["J"] = 6
-init_graph["P"]["Q"] = 3
-init_graph["P"]["B"] = 11
-init_graph["P"]["C"] = 6
-init_graph["Q"]["J"] = 10
-init_graph["Q"]["P"] = 3
-init_graph["Q"]["B"] = 6
-init_graph["Q"]["C"] = 4
-init_graph["B"]["A"] = 13
-init_graph["B"]["P"] = 11
-init_graph["B"]["Q"] = 6
-init_graph["B"]["E"] = 3
-init_graph["C"]["P"] = 6
-init_graph["C"]["Q"] = 4
-init_graph["C"]["E"] = 8
-init_graph["E"]["B"] = 3
-init_graph["E"]["C"] = 8
 
-	
-graph = Graph(nodes, init_graph)
+
+# init_graph["J"]["A"] = 5
+# init_graph["J"]["P"] = 6
+# init_graph["J"]["Q"] = 10
+# init_graph["A"]["J"] = 5
+# init_graph["A"]["B"] = 13
+# init_graph["P"]["J"] = 6
+# init_graph["P"]["Q"] = 3
+# init_graph["P"]["B"] = 11
+# init_graph["P"]["C"] = 6
+# init_graph["Q"]["J"] = 10
+# init_graph["Q"]["P"] = 3
+# init_graph["Q"]["B"] = 6
+# init_graph["Q"]["C"] = 4
+# init_graph["B"]["A"] = 13
+# init_graph["B"]["P"] = 11
+# init_graph["B"]["Q"] = 6
+# init_graph["B"]["E"] = 3
+# init_graph["C"]["P"] = 6
+# init_graph["C"]["Q"] = 4
+# init_graph["C"]["E"] = 8
+# init_graph["E"]["B"] = 3
+# init_graph["E"]["C"] = 8
+
+graph = Graph(nodes, new_graph)
 dijkstra = Dijkstra()
 #previous_nodes, shortest_path = dijkstra.dijkstra_algorithm(graph, "Reykjavik")
-previous_nodes, shortest_path = dijkstra.dijkstra_algorithm(graph, "J")
+previous_nodes, shortest_path = dijkstra.dijkstra_algorithm(graph, 'J')
 #dijkstra.print_result(previous_nodes, shortest_path, start_node="Reykjavik", target_node="Athens")
 dijkstra.print_result(previous_nodes, shortest_path, start_node="J", target_node="E")
